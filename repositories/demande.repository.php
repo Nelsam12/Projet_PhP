b5ee65480a0463697ffa542cbf7de9350cc4f191
Nelsam12 <elolo-nelsam.gumedzoe@ism.edu.sn>
2023-12-01 00:58:42 +0000

    init

---
+++ repositories/demande.repository.php
@@ -0,0 +1,20 @@
+<?php
+   //definir un tableau de 5  demandes
+     //une demande est caracterise
+        //numero, chaine genere aleatoirement
+         //date 
+         //Etat(Encours,Accepte,Refuse)
+         //Type (Suspension ou Annulation)
+         //motif
+         //etudiant (matricule,nom,prenom,
+                //dateNaiss(Date php),
+                //classe(filiere,niveau,code))
+
+// fonction qui retourne le tableau de demande
+// fonction qui retourne le tableau de demande par type
+// fonction qui retourne le tableau de demande par etat
+
+
+
+
+?>
